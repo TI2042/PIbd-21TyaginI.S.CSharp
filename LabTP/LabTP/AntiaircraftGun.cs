4712c49935da9de9b579c102df3d2382ca7cc80e
TI2042 <tis3.1415@gmail.com>
2019-10-09 17:35:17 +0400

    add lab1

---
+++ LabTP/LabTP/AntiaircraftGun.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabTP
+{
+    class AntiaircraftGun
+    {
+
+
+        private float StartX;
+        private float StartY;
+        private int PictureWight;
+        private int PictureHight;
+        private const int GunWidth = 100;
+        private const int GunHidth = 60;
+        public int MaxSpeed { private set; get; }
+        public float Weight { private set; get; }
+        public Color MainColor { private set; get; }
+        public Color DopColor { private set; get; }
+        public bool FrontArmor { private set; get; }// передняя броня
+        public bool MuzzleBraker { private set; get; }// дульный тормоз
+        public bool Radar { private set; get; }//радар
+        public AntiaircraftGun(int maxSped, float weight, Color mainColor, Color dopColor,
+            bool frontArmor, bool muzzleBraker, bool radar)
+        {
+            MaxSpeed = maxSped;
+            Weight = weight;
+            MainColor = mainColor;
+            DopColor = dopColor;
+            FrontArmor = frontArmor;
+            MuzzleBraker = muzzleBraker;
+            Radar = radar;
+        }
+        public void SetPosition(int x, int y, int width, int height)
+        {
+            StartX = x;
+            StartY = y;
+            PictureWight = width;
+            PictureHight = height;
+        }
+        public void MoveGun(Direction direction)
+        {
+            float step = MaxSpeed * 100 / Weight;
+            switch (direction)
+            {
+                case Direction.Right:
+                    if (StartX + step < PictureWight - GunWidth)
+                    {
+                        StartX += step;
+                    }
+                    break;
+                case Direction.Left:
+                    if (StartX - step > 0)
+                    {
+                        StartX -= step;
+                    }
+                    break;
+                case Direction.Up:
+                    if (StartY - step > 0)
+                    {
+                        StartY -= step;
+                    }
+                    break;
+                case Direction.Down:
+                    if (StartY + step < PictureHight - GunHidth)
+                    {
+                        StartY += step;
+                    }
+                    break;
+
+            }
+
+        }
+        public void DrawGun(Graphics g)
+        {
+
+            Brush br = new SolidBrush(MainColor);
+            Point point1 = new Point((int)StartX - 40, (int)StartY + 10);
+            Point point2 = new Point((int)StartX + 40, (int)StartY + 10);
+            Point point3 = new Point((int)StartX + 45, (int)StartY + 25);
+            Point point4 = new Point((int)StartX - 45, (int)StartY + 25);
+            Point[] trapezePoints = { point1, point2, point3, point4 };
+            g.FillPolygon(br, trapezePoints);
+
+
+
+            Brush brBc = new SolidBrush(Color.Black);
+            g.FillEllipse(brBc, StartX - 30, StartY + 20, 15, 15);
+            g.FillEllipse(brBc, StartX + 20, StartY + 20, 15, 15);
+            g.FillRectangle(brBc, StartX, StartY, 10, 10);
+
+            Point pointG1 = new Point((int)StartX + 15, (int)StartY - 5);
+            Point pointG2 = new Point((int)StartX + 35, (int)StartY - 25);
+            Point pointG3 = new Point((int)StartX + 40, (int)StartY - 20);
+            Point pointG4 = new Point((int)StartX + 20, (int)StartY);
+            Point[] Guns = { pointG1, pointG2, pointG3, pointG4 };
+            g.FillPolygon(brBc, Guns);
+
+            if (Radar)
+            {
+                Rectangle rect = new Rectangle((int)StartX + 10, (int)StartY - 25, 10, 10);
+                g.FillPie(br, rect, 250, 180);
+            }
+
+            if (FrontArmor)
+            {
+                Brush br2 = new SolidBrush(DopColor);
+                Point pointF1 = new Point((int)StartX, (int)StartY - 15);
+                Point pointF2 = new Point((int)StartX + 20, (int)StartY - 15);
+                Point pointF3 = new Point((int)StartX + 35, (int)StartY + 10);
+                Point pointF4 = new Point((int)StartX + 15, (int)StartY + 10);
+                Point[] parall = { pointF1, pointF2, pointF3, pointF4 };
+                g.FillPolygon(br2, parall);
+            }
+            if (MuzzleBraker)
+            {
+                Brush brGr = new SolidBrush(Color.Gray);
+                Point pointM4 = new Point((int)StartX + 35, (int)StartY - 25);
+                Point pointM2 = new Point((int)StartX + 45, (int)StartY - 35);
+                Point pointM3 = new Point((int)StartX + 40, (int)StartY - 20);
+                Point pointM1 = new Point((int)StartX + 50, (int)StartY - 30);
+                Point[] mB = { pointM1, pointM2, pointM3, pointM4 };
+                g.FillPolygon(brGr, mB);
+            }
+
+
+
+
+
+
+
+
+        }
+
+    }
+}
---
+++ LabTP/LabTP/AntiaircraftGunForm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LabTP
+{
+    public partial class AntiaircraftGunForm : Form
+    {
+        private AntiaircraftGun gun;
+        public AntiaircraftGunForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Draw()
+        {
+            Bitmap bmp = new Bitmap(pictureBoxGun.Width, pictureBoxGun.Height);
+            Graphics gr = Graphics.FromImage(bmp);
+            gun.DrawGun(gr);
+            pictureBoxGun.Image = bmp;
+        }
+        private void ButtonCreate_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            gun = new AntiaircraftGun(rnd.Next(100, 300), rnd.Next(1000, 2000), Color.DarkGreen, Color.Gray, true, true, true);
+            gun.SetPosition(rnd.Next(10, 100), rnd.Next(10, 100), pictureBoxGun.Width, pictureBoxGun.Height);
+            Draw();
+        }
+        private void ButtonMove_Click(object sender, EventArgs e)
+        {
+            string name = (sender as Button).Name;
+            switch (name)
+            {
+                case "buttonUp":
+                    gun.MoveGun(Direction.Up);
+                    break;
+                case "buttonDown":
+                    gun.MoveGun(Direction.Down);
+                    break;
+                case "buttonLeft":
+                    gun.MoveGun(Direction.Left);
+                    break;
+                case "buttonRight":
+                    gun.MoveGun(Direction.Right);
+                    break;
+            }
+            Draw();
+        }
+
+
+    }
+}
